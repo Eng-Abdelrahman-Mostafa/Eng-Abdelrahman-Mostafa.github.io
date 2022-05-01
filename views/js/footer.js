27369c957d7bd9659a0d83402c812c4e1420499d
Eng-Abdelrahman-Mostafa <abdelrahmanmostafa785@gmail.com>
2022-05-01 12:36:43 +0200

    20210533

--- views/js/footer.js
+++ views/js/footer.js
@@ -1,19 +1,19 @@
 let footer= document.querySelector(".footer");
 
-if(theme_key == 1){
+if(theme_key == 1 ){
     footer.innerHTML= "<footer class=\"\">\n" +
         "    <div class=\"container\">\n" +
         "        <div class=\"row\">\n" +
-        "            <div class=\"col-lg-3 col-md-6\">\n" +
+        "            <div class=\"col-lg-4 col-md-6\">\n" +
         "                <div class=\"box\">\n" +
         "                    <div class=\"brand\">\n" +
         "                        <img src=\"img/logo.png\" alt=\"\" />\n" +
-        "                        <span> دكان </span>\n" +
+        "                        <span class='dokan'> دكان </span>\n" +
         "                    </div>\n" +
-        "                    <p>\n" +
-        "                        موقع دكان للبيع و الشراء اون لاين تصميم فريق طلاب كلية\n" +
-        "                        حاسبات وذكاء اصطناعي جامعة حلوان\n" +
-        "                    </p>\n" +
+        "                </div>\n" +
+        "            </div>\n" +
+        "            <div class=\"col-lg-4 col-md-6\">\n" +
+        "                <div class=\"box-two\">\n" +
         "                    <div class=\"icons\">\n" +
         "                        <span><i class=\"fa fa-facebook\"></i></span>\n" +
         "                        <span><i class=\"fa fa-youtube\"></i></span>\n" +
@@ -22,43 +22,9 @@ if(theme_key == 1){
         "                    </div>\n" +
         "                </div>\n" +
         "            </div>\n" +
-        "            <div class=\"col-lg-3 col-md-6\">\n" +
-        "                <div class=\"box-two\">\n" +
-        "                    <h3>الفئات</h3>\n" +
-        "                    <ul>\n" +
-        "                        <li>الالكترونيات</li>\n" +
-        "                        <li>موبايلات</li>\n" +
-        "                        <li>العطور</li>\n" +
-        "                        <li>الموضة</li>\n" +
-        "                        <li>عرض اليوم</li>\n" +
-        "                        <li>العاب الكترونية</li>\n" +
-        "                        <li>أجهزة كمبيوتر</li>\n" +
-        "                        <li>مستلزمات منزلية</li>\n" +
-        "                    </ul>\n" +
-        "                </div>\n" +
-        "            </div>\n" +
-        "            <div class=\"col-lg-3 col-md-6\">\n" +
-        "                <div class=\"box-two\">\n" +
-        "                    <h3>الصفحات</h3>\n" +
-        "                    <ul>\n" +
-        "                        <li>الالكترونيات</li>\n" +
-        "                        <li>موبايلات</li>\n" +
-        "                        <li>العطور</li>\n" +
-        "                        <li>الموضة</li>\n" +
-        "                        <li>عرض اليوم</li>\n" +
-        "                        <li>العاب الكترونية</li>\n" +
-        "                        <li>أجهزة كمبيوتر</li>\n" +
-        "                        <li>مستلزمات منزلية</li>\n" +
-        "                    </ul>\n" +
-        "                </div>\n" +
-        "            </div>\n" +
-        "            <div class=\"col-lg-3 col-md-6\">\n" +
+        "\n" +
+        "            <div class=\"col-lg-4 col-md-6\">\n" +
         "                <div class=\"box\">\n" +
-        "                    <h3>يمكنك التواصل مع خدمة عملائنا</h3>\n" +
-        "                    <p>\n" +
-        "                        يمكنك التواصل مع خدمه العملاء بأكثر من طريقة لحل مشكلتك\n" +
-        "                        او تلقي أقتراحك\n" +
-        "                    </p>\n" +
         "                    <div class=\"contact-us\">\n" +
         "                        <div class=\"content\">\n" +
         "                            <p>تواصل على الخط الساخن</p>\n" +
@@ -68,17 +34,6 @@ if(theme_key == 1){
         "                            <i class=\"fa fa-headphones-simple\"></i>\n" +
         "                        </div>\n" +
         "                    </div>\n" +
-        "                    <a href=\"#\">\n" +
-        "                        <div class=\"download\">\n" +
-        "                            <div>\n" +
-        "                                <span> حمل التطبيق </span>\n" +
-        "                                <span>google play</span>\n" +
-        "                            </div>\n" +
-        "                            <div>\n" +
-        "                                <i class=\"fa-brands fa-google-play\"></i>\n" +
-        "                            </div>\n" +
-        "                        </div>\n" +
-        "                    </a>\n" +
         "                </div>\n" +
         "            </div>\n" +
         "        </div>\n" +
@@ -86,7 +41,47 @@ if(theme_key == 1){
         "</footer>\n" +
         "<div class=\"copy-right\">\n" +
         "    <p>حقوق الطبع والنشر © لعام 2022 محفوظة لموقع <span>دكان</span></p>\n" +
-        "</div>";
+        "</div>" ;
 }else {
-    footer.innerHTML = "";
+    footer.innerHTML= "<footer class=\"\">\n" +
+        "    <div class=\"container\">\n" +
+        "        <div class=\"row\">\n" +
+        "            <div class=\"col-lg-4 col-md-6\">\n" +
+        "                <div class=\"box\">\n" +
+        "                    <div class=\"brand\">\n" +
+        "                        <img src=\"img/logo2.png\" alt=\"\" />\n" +
+        "                        <span class='dokan'> دكان </span>\n" +
+        "                    </div>\n" +
+        "                </div>\n" +
+        "            </div>\n" +
+        "            <div class=\"col-lg-4 col-md-6\">\n" +
+        "                <div class=\"box-two\">\n" +
+        "                    <div class=\"icons\">\n" +
+        "                        <span><i class=\"fa fa-facebook\"></i></span>\n" +
+        "                        <span><i class=\"fa fa-youtube\"></i></span>\n" +
+        "                        <span><i class=\"fa fa-instagram\"></i></span>\n" +
+        "                        <span><i class=\"fa fa-twitter\"></i></span>\n" +
+        "                    </div>\n" +
+        "                </div>\n" +
+        "            </div>\n" +
+        "\n" +
+        "            <div class=\"col-lg-4 col-md-6\">\n" +
+        "                <div class=\"box\">\n" +
+        "                    <div class=\"contact-us\">\n" +
+        "                        <div class=\"content\">\n" +
+        "                            <p>تواصل على الخط الساخن</p>\n" +
+        "                            <p>20123456789+</p>\n" +
+        "                        </div>\n" +
+        "                        <div class=\"icon\">\n" +
+        "                            <i class=\"fa fa-headphones-simple\"></i>\n" +
+        "                        </div>\n" +
+        "                    </div>\n" +
+        "                </div>\n" +
+        "            </div>\n" +
+        "        </div>\n" +
+        "    </div>\n" +
+        "</footer>\n" +
+        "<div class=\"copy-right\">\n" +
+        "    <p>حقوق الطبع والنشر © لعام 2022 محفوظة لموقع <span>دكان</span></p>\n" +
+        "</div>" ;
 }
--- views/js/header.js
+++ views/js/header.js
@@ -1,1 +1,110 @@
 let header= document.querySelector(".header");
+
+if(theme_key==2){
+    header.innerHTML="<div class=\"parent\">\n" +
+        "    <div class=\"top-bar\">\n" +
+        "        <div class=\"container-fluid\">\n" +
+        "            <div class=\"box\">\n" +
+        "                <div class=\"right-box\">\n" +
+        "                    <div class=\"item\">\n" +
+        "                        <i class=\"fa-brands fa-facebook-square\"></i>\n" +
+        "                    </div>\n" +
+        "                    <div class=\"item\">\n" +
+        "                        <i class=\"fa-brands fa-instagram\"></i>\n" +
+        "                    </div>\n" +
+        "                    <div class=\"item\">\n" +
+        "                        <i class=\"fa-brands fa-twitter-square\"></i>\n" +
+        "                    </div>\n" +
+        "                    <div class=\"item\">\n" +
+        "                        <i class=\"fa-brands fa-linkedin\"></i>\n" +
+        "                    </div>\n" +
+        "                </div>\n" +
+        "                <div class=\"left-box\">\n" +
+        "                    <div class=\"item\">\n" +
+        "                        <span>20123456789+</span>\n" +
+        "                        <i class=\"fa-solid fa-phone\"></i>\n" +
+        "                    </div>\n" +
+        "                    <div class=\"item\">\n" +
+        "                        <span>example@gmail.com</span>\n" +
+        "                        <i class=\"fa-solid fa-envelope\"></i>\n" +
+        "                    </div>\n" +
+        "                </div>\n" +
+        "            </div>\n" +
+        "        </div>\n" +
+        "    </div>\n" +
+        "    <nav class=\"navbar navbar-expand-lg navbar-light nav1\">\n" +
+        "        <div class=\"container-fluid nav-mar\">\n" +
+        "            <a class=\"navbar-brand\" href=\"#\"\n" +
+        "            ><img src=\"img/logo2.png\" alt=\"\" />\n" +
+        "                <span>دكان</span></a\n" +
+        "            >\n" +
+        "            <div\n" +
+        "                    class=\"collapse navbar-collapse\"\n" +
+        "                    id=\"navbarSupportedContent\"\n" +
+        "            >\n" +
+        "                <ul class=\"navbar-nav ml-auto mb-2 mb-lg-0\">\n" +
+        "                    <li class=\"nav-item\">\n" +
+        "                        <a class=\"nav-link active\" aria-current=\"page\" href=\"#\"\n" +
+        "                        >جميع الفئات</a\n" +
+        "                        >\n" +
+        "                    </li>\n" +
+        "                    <li class=\"nav-item\">\n" +
+        "                        <a class=\"nav-link\" href=\"#\"> الموضة</a>\n" +
+        "                    </li>\n" +
+        "                    <li class=\"nav-item\">\n" +
+        "                        <a class=\"nav-link\" href=\"#\">السوبر ماركت</a>\n" +
+        "                    </li>\n" +
+        "                    <li class=\"nav-item\">\n" +
+        "                        <a class=\"nav-link\" href=\"#\">العطور</a>\n" +
+        "                    </li>\n" +
+        "                    <li class=\"nav-item\">\n" +
+        "                        <a class=\"nav-link\" href=\"#\">موبايلات</a>\n" +
+        "                    </li>\n" +
+        "                </ul>\n" +
+        "                <div class=\"nav-icon\">\n" +
+        "                     <span class=\"shopping\"\n" +
+        "                     ><i class=\"fa fa-shopping-cart\" aria-hidden=\"true\"></i>\n" +
+        "                     </span>\n" +
+        "                    <a href=\"#\">\n" +
+        "                        حسابي\n" +
+        "                        <i class=\"fa fa-user-circle\" aria-hidden=\"true\"></i>\n" +
+        "                    </a>\n" +
+        "                </div>\n" +
+        "            </div>\n" +
+        "            <form class=\"d-flex\">\n" +
+        "                <input\n" +
+        "                        class=\"form-control me-2\"\n" +
+        "                        type=\"search\"\n" +
+        "                        placeholder=\"ابحث هنا\"\n" +
+        "                        aria-label=\"Search\"\n" +
+        "                />\n" +
+        "                <!-- <button class=\"btn btn-outline-success\" type=\"submit\">Search</button> -->\n" +
+        "            </form>\n" +
+        "\n" +
+        "            <div class=\"nav-icon lol\">\n" +
+        "                  <span class=\"shopping\"\n" +
+        "                  ><i class=\"fa fa-shopping-cart\" aria-hidden=\"true\"></i>\n" +
+        "                  </span>\n" +
+        "                <a href=\"#\">\n" +
+        "                    حسابي\n" +
+        "                    <i class=\"fa fa-user-circle\" aria-hidden=\"true\"></i>\n" +
+        "                </a>\n" +
+        "            </div>\n" +
+        "\n" +
+        "            <button\n" +
+        "                    class=\"navbar-toggler\"\n" +
+        "                    type=\"button\"\n" +
+        "                    data-bs-toggle=\"collapse\"\n" +
+        "                    data-bs-target=\"#navbarSupportedContent\"\n" +
+        "                    aria-controls=\"navbarSupportedContent\"\n" +
+        "                    aria-expanded=\"false\"\n" +
+        "                    aria-label=\"Toggle navigation\"\n" +
+        "            >\n" +
+        "                <i class=\"fa-solid fa-bars\"></i>\n" +
+        "            </button>\n" +
+        "        </div>\n" +
+        "    </nav>\n" +
+        "</div>";
+}else if(theme_key==1){
+
+}
