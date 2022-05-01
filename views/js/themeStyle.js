da9f23440318320036fead17d7e9c84801763f77
Eng-Abdelrahman-Mostafa <abdelrahmanmostafa785@gmail.com>
2022-05-01 11:23:14 +0200

    20210533

--- views/js/themeStyle.js
+++ views/js/themeStyle.js
@@ -1,3 +1,12 @@
-if(theme_key == 1){
+var head = document.getElementsByTagName('HEAD')[0];
+var link = document.createElement('link');
 
+link.rel = 'stylesheet';
+link.type = 'text/css';
+if(theme_key==1){
+    link.href = 'css/style.min.css';
+}else if(theme_key ==2){
+    link.href = 'css/style2.min.css';
 }
+
+head.appendChild(link);
